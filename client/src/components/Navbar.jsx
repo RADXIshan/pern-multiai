02dbaed9aba713f85e0df4332ac2cbee995bad81
Ishan Roy <ishanroy3118107@gmail.com>
2025-07-30 20:09:57 +0530

    Styling Changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,12 +9,17 @@ const Navbar = () => {
   const { openSignIn } = useClerk();
 
   return (
-    <div id="navbar" className='z-40 fixed top-2 inset-x-0 backdrop-blur-lg flex
-    justify-between items-center py-2 px-5 w-[60%] sm:px-20 xl:px-10 border-primary border-2 rounded-[20vw] shadow-gray-400 shadow-lg mx-auto'>
+    <div 
+        id="navbar" 
+        className='z-40 fixed top-2 inset-x-0 mx-auto flex justify-between items-center rounded-full border-2 border-primary bg-white/30 backdrop-blur-lg shadow-lg 
+        w-[80%] p-2 px-10
+        md:w-4/5 
+        lg:w-3/4'
+    >
         <img
             src={assets.logo}
             alt="logo"
-            className='w-28 sm:w-40 xl:w-44 cursor-pointer transition-transform hover:scale-105'
+            className='w-28 cursor-pointer transition-transform hover:scale-105 md:w-36'
             onClick={() => navigate("/")}
         />
 
@@ -24,9 +29,10 @@ const Navbar = () => {
                 (
                     <button
                         onClick={openSignIn}
-                        className='flex items-center gap-2 bg-primary text-white text-sm sm:text-base font-medium px-6 sm:px-8 py-2.5 sm:py-3 rounded-full shadow-lg hover:brightness-110 transition-all duration-200 cursor-pointer'>
-                        Get Started
-                        <ArrowRight className='w-4 h-4 sm:w-5 sm:h-5' />
+                        className='flex items-center gap-2 rounded-full bg-primary px-4 py-4 text-sm font-medium text-white shadow-lg transition-all duration-200 hover:brightness-110 active:scale-95 md:px-6 md:py-4.5'
+                    >
+                        <span className="hidden sm:inline">Get Started</span>
+                        <ArrowRight className='h-5 w-5' />
                     </button>
                 )
         }
